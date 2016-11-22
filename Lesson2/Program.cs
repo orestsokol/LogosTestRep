e636418a55eda5651216a645a79cf759e8b4fff1
Sokol Orest <orest.sokol@gmail.com>
2016-11-22 20:37:29 +0200

    Added lesson 2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Please write numbers for action");
+          int firstNum = int.Parse(Console.ReadLine());
+          int secondNum = int.Parse(Console.ReadLine());
+
+          Console.WriteLine("Please write number of action to do: \n 1 : + \n 2 : - \n 3 : * \n 4 : /");
+          int act = int.Parse(Console.ReadLine());
+
+          switch(act)
+          {
+              case 1:
+                  Console.WriteLine(firstNum + secondNum);
+                  break;
+              case 2:
+                  Console.WriteLine(firstNum - secondNum);
+                  break;
+              case 3:
+                  Console.WriteLine(firstNum * secondNum);
+                  break;
+              case 4:
+                  Console.WriteLine(firstNum / secondNum);
+                  break;
+          }
+
+          */
+            /*
+            Console.WriteLine("Please enter year");
+            int year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0 && year % 400 != 0)
+                Console.WriteLine("This year is leap ");
+            else
+                Console.WriteLine("This year is not leap ");
+            */
+        }
+    }
+}
