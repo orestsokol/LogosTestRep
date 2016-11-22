a0e33d87264f8767ade014f9474a6904d8e7cb08
Sokol Orest <orest.sokol@gmail.com>
2016-11-22 21:01:54 +0200

    Added homework

---
+++ Homework1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your mark of History subject:");
+            int subjHistory = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your mark of Math subject:");
+            int subjMath = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your mark of Physo subject:");
+            int subjPhyso = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(" The avarage value of your subject is : {0}", (subjHistory + subjMath + subjPhyso)/3 );
+        }
+    }
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the radious of circule");
+            int radious = int.Parse(Console.ReadLine());
+            double circuit = 2 * Math.PI * radious; 
+
+            Console.WriteLine("The circuit is: " + circuit);
+        }
+    }
+}
