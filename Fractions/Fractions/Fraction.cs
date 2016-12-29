df753595235a8e08cdc94b3429722d044453c10a
Sokol Orest <orest.sokol@gmail.com>
2016-12-29 20:14:25 +0200

    Add work and homework

--- Fractions/Fractions/Fraction.cs
+++ Fractions/Fractions/Fraction.cs
@@ -6,40 +6,124 @@ using System.Threading.Tasks;
 
 namespace Fractions
 {
-    class Fraction
+    class Fraction:FullNum
     {
-        private float n;
-        private double m;
-        public float N { get; set;}
-        public double M { get; set; }
+        private int n;
+        private float m;
+        private float fullNum;
 
-        public Fraction(float fN, double fM)
+        public float FullNum 
         {
-            N = fN;
-            M = fM;
+            get {
+                return fullNum;
+            }
+            set {
+                fullNum = value;
+            }
         }
 
-        public float Add(float fN, double fM)
+        public int N
+        {
+            get
+            {
+                return n;
+            }
+            set
+            {
+                n = value;
+            }
+        }
+
+        public float M
+        {
+            get
+            {
+                return m;
+            }
+            set
+            {
+                m = value;
+            }
+        }
+ 
+        public Fraction(float fN)
+        {
+            FullNum = fN;
+        }
+
+        public float Add(float fN, float fM)
         {
             float result = 0;
+            int resultInt = 0;
+            int fNPart1 = (int)fN;
+            float fNPart2 = (int)fN - fN;
+            int fMPart1 = (int)fM;
+            float fMPart2 = (int)fM - fM;
+
+            result = fNPart2 + fMPart2;
+             if(result > 1)
+             {
+                 resultInt += 1;
+                 result -= 1;
+             }
+
+            resultInt = fNPart1 + fMPart1;
+            result += resultInt;
             return result;
         }
 
-         public float Divide(float fN, double fM)
+        public float Divide(float fN, float fM)
         {
             float result = 0;
+            int resultInt = 0;
+            int fNPart1 = (int)fN;
+            float fNPart2 = (int)fN - fN;
+            int fMPart1 = (int)fM;
+            float fMPart2 = (int)fM - fM;
+
+            result = fNPart2 + fMPart2;
+            if (result > 1)
+            {
+                resultInt += 1;
+                result -= 1;
+            }
+
+            resultInt = fNPart1 + fMPart1;
+            result += resultInt;
             return result;
         }
 
-        public float Multiply(float fN, double fM)
+        public float Multiply(float fN, float fM)
         {
             float result = 0;
+            int resultInt = 0;
+            int fNPart1 = (int)fN;
+            float fNPart2 = (int)fN - fN;
+            int fMPart1 = (int)fM;
+            float fMPart2 = (int)fM - fM;
+
+            result = fNPart2 * fMPart2;
+            if (result > 1)
+            {
+                resultInt += 1;
+                result -= 1;
+            }
+
+            resultInt = fNPart1 * fMPart1;
+            result += resultInt;
             return result;
         }
 
-         public bool Compare(float fN, double fM)
+        public bool Compare(float fN, float fM)
         {
             bool result = false;
+            if (fN > fM)
+                result = true;
+            else if(fM > fN)
+                result = true;
+            else (fM == fN)
+                result = false;
+
             return result;
         }
 
---
+++ Kontr/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontr
+{
+    class WorkString
+    {
+        private Stack<string> _stack;
+        public Stack<string> Stack { get; set; }
+
+        public WorkString()
+        {
+        }
+
+        public string ReplaceWords(string str)
+        {
+            string result = "";
+            string[] arr = str.Split(' ');
+
+            _stack = new Stack<string>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                _stack.Push(arr[i]);
+            }
+
+            foreach (Object obj in _stack)
+            {
+                result += obj + "\n";
+            }
+
+            return result;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some words");
+            string text = Console.ReadLine();
+            WorkString word = new WorkString();
+            Console.WriteLine(word.ReplaceWords(text));
+        }
+    }
+}
